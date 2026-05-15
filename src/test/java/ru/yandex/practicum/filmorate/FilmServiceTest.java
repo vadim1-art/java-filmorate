b53645c4c31ab0e58b32bfc5bb0bfb57f6edfe26
vadim_gotovec <vadimgotovec1@gmail.com>
2026-05-15 18:27:56 +0300

    # 1.1.0

--- src/test/java/ru/yandex/practicum/filmorate/FilmServiceTest.java
+++ src/test/java/ru/yandex/practicum/filmorate/FilmServiceTest.java
@@ -6,7 +6,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.DirtiesContext;
-import ru.yandex.practicum.filmorate.exception.DuplicatedDataException;
 import ru.yandex.practicum.filmorate.exception.NotFoundException;
 import ru.yandex.practicum.filmorate.model.Film;
 import ru.yandex.practicum.filmorate.model.Mpa;
@@ -32,16 +31,16 @@ class FilmServiceTest {
         Film created = filmService.create(film);
 
         assertNotNull(created.getId());
-        assertEquals(1L, created.getId());
+        assertEquals("Interstellar", created.getName());
     }
 
     @Test
-    void createFilmWithDuplicateNameShouldThrowException() {
-        filmService.create(new Film(null, "Title", "desc",
+    void createTwoFilmsWithSameNameShouldBeAllowed() {
+        filmService.create(new Film(null, "Avatar", "desc",
                 LocalDate.now(), 100L, new Mpa(1, "G"), null));
 
-        assertThrows(DuplicatedDataException.class, () ->
-                filmService.create(new Film(null, "Title", "desc",
+        assertDoesNotThrow(() ->
+                filmService.create(new Film(null, "Avatar", "another desc",
                         LocalDate.now(), 100L, new Mpa(1, "G"), null))
         );
     }
@@ -49,10 +48,12 @@ class FilmServiceTest {
     @Test
     void updateExistingFilmShouldChangeFields() {
         Film original = filmService.create(new Film(null, "Original", "Desc",
-                LocalDate.of(2000, 1, 1), 100L, new Mpa(1, "G"), null));
+                LocalDate.of(2000, 1, 1),
+                100L, new Mpa(1, "G"), null));
 
         Film update = new Film(original.getId(), "Updated", "New desc",
-                LocalDate.of(2001, 2, 2), 150L, new Mpa(2, "PG"), null);
+                LocalDate.of(2001, 2, 2),
+                150L, new Mpa(2, "PG"), null);
 
         Film result = filmService.update(update);
 
@@ -65,16 +66,5 @@ class FilmServiceTest {
         Film film = new Film(999L, "Name", "Desc",
                 LocalDate.now(), 120L, new Mpa(1, "G"), null);
         assertThrows(NotFoundException.class, () -> filmService.update(film));
-    }
-
-    @Test
-    void updateFilmOwnNameDifferentCaseShouldNotThrowException() {
-        Film original = filmService.create(new Film(null, "Inception", "Desc",
-                LocalDate.now(), 100L, new Mpa(1, "G"), null));
-        Film update = new Film(original.getId(), "INCEPTION", "Desc",
-                LocalDate.now(), 100L, new Mpa(1, "G"), null);
-
-        assertDoesNotThrow(() -> filmService.update(update));
-        assertEquals("INCEPTION", filmService.getFilms().iterator().next().getName());
     }
 }
--- src/test/java/ru/yandex/practicum/filmorate/UserServiceTest.java
+++ src/test/java/ru/yandex/practicum/filmorate/UserServiceTest.java
@@ -11,7 +11,6 @@ import ru.yandex.practicum.filmorate.model.User;
 import ru.yandex.practicum.filmorate.service.UserService;
 
 import java.time.LocalDate;
-import java.util.Collection;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -25,57 +24,26 @@ class UserServiceTest {
 
     @Test
     void createValidUserShouldReturnUserWithId() {
-        User user = new User(null, "test@ya.ru",
-                "login", "name", LocalDate.now().minusYears(20));
+        User user = new User(null, "test@ya.ru", "login", "name",
+                LocalDate.now().minusYears(20));
         User created = userService.create(user);
 
         assertNotNull(created.getId());
-        assertEquals(1L, created.getId());
     }
 
     @Test
     void createUserWithEmptyNameShouldUseLoginAsName() {
-        User user = new User(null, "test@mail.ru",
-                "MyLogin", "", LocalDate.now().minusYears(20));
+        User user = new User(null, "test@mail.ru", "MyLogin", "",
+                LocalDate.now().minusYears(20));
         User created = userService.create(user);
 
         assertEquals("MyLogin", created.getName());
     }
 
     @Test
-    void addFriendShouldWorkOneWay() {
-        User user1 = userService.create(new User(null, "u1@ya.ru",
-                "l1", "n1", LocalDate.now().minusYears(20)));
-        User user2 = userService.create(new User(null, "u2@ya.ru",
-                "l2", "n2", LocalDate.now().minusYears(20)));
-
-        userService.addFriend(user1.getId(), user2.getId());
-
-        Collection<User> friendsOf1 = userService.getFriends(user1.getId());
-        Collection<User> friendsOf2 = userService.getFriends(user2.getId());
-
-        assertEquals(1, friendsOf1.size(),
-                "У первого пользователя должен быть 1 друг");
-        assertEquals(0, friendsOf2.size(),
-                "У второго пользователя должно быть 0 друзей (односторонняя связь)");
-    }
-
-    @Test
-    void updateExistingUserShouldChangeFields() {
-        User original = userService.create(new User(null, "old@ya.ru",
-                "old", "Old", LocalDate.now().minusYears(20)));
-        User update = new User(original.getId(), "new@ya.ru",
-                "new", "New", LocalDate.now().minusYears(20));
-
-        User result = userService.update(update);
-
-        assertEquals("new@ya.ru", result.getEmail());
-        assertEquals("New", result.getName());
-    }
-
-    @Test
     void updateNonExistentUserShouldThrowNotFoundException() {
-        User user = new User(999L, "a@b.com", "login", "name", LocalDate.now());
+        User user = new User(999L, "a@b.com", "login", "name",
+                LocalDate.now().minusYears(10));
         assertThrows(NotFoundException.class, () -> userService.update(user));
     }
 }
